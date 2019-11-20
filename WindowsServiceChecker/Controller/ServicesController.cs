40770c2a8277c49850dd30352095041ae1cffbb8
Sachin Bisht <sbisht@cogitate.us>
2019-11-20 12:53:33 +0530

    Git repository first push for WindowsServiceChecker API.

---
+++ WindowsServiceChecker/Controller/ServicesController.cs
@@ -0,0 +1,41 @@
+﻿using System.ServiceProcess;
+using System.Net.Http;
+using System.Web.Http;
+using System.Net;
+using System;
+
+namespace WindowsServiceChecker.Controller
+{
+    public class ServicesController : ApiController
+    {
+        // GET api/<controller>/<action>
+        [HttpGet]
+        public HttpResponseMessage Status(string service)
+        {
+            if(string.IsNullOrEmpty(service))
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            ServiceController sc = new ServiceController(service);
+            
+            try
+            {
+                switch (sc.Status)
+                {
+                    case ServiceControllerStatus.Running:
+                        return Request.CreateResponse(HttpStatusCode.Accepted);
+                        //Add other statuses and their responses.
+                    default:
+                        return Request.CreateResponse(HttpStatusCode.ServiceUnavailable);
+                }
+            }
+            catch(Exception notAService)
+            {
+                Console.WriteLine("Not a service:" + notAService);
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+            
+        }
+    }
+}
---
+++ WindowsServiceChecker/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Http;
+using Owin;
+
+namespace WindowsServiceChecker
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "{action}/{service}",
+                defaults: new { id = RouteParameter.Optional, controller="Services" }
+            );
+
+            //JSON Formatters [Uncomment if required]
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            //config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            //config.Formatters.JsonFormatter.SerializerSettings.DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc;
+
+            app.UseWebApi(config);
+        }
+    }
+}
